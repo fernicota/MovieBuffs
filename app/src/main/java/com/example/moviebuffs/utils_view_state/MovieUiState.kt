18114ee36196f92a184f3c59bad6fc9d00d0c073
Fernando Cota <frarreolacota1@buffs.wtamu.edu>
2023-12-07 18:26:08 -0600

    Ready

--- app/src/main/java/com/example/moviebuffs/utils_view_state/MovieUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviebuffs.utils_view_state
-
-import com.example.moviebuffs.network.MoviePhoto
-
-data class MoviesUiState(
-    val moviesList: List<MoviePhoto> = emptyList(),
-    val currentMovie: MoviePhoto? = null,
-    val isShowingListPage: Boolean = true
-)
--- app/src/main/java/com/example/moviebuffs/utils_view_state/WindowStateUtils.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moviebuffs.utils_view_state
-
-enum class MovieContentType {
-    LIST_ONLY, LIST_AND_DETAIL
-}
